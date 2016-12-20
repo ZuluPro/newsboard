8ea48e1873e992531fec168204f90f11eae8f2d3
ZuluPro <montheanthony@hotmail.com>
2016-12-19 20:55:12 -0500

    Put version

---
+++ newsboard/__init__.py
@@ -0,0 +1,7 @@
+"Gather many feeds in one"
+VERSION = (0, 0, 1)
+__version__ = '.'.join([str(i) for i in VERSION])
+__author__ = 'Anthony Monthe (ZuluPro)'
+__email__ = 'anthony.monthe@gmail.com'
+__url__ = 'https://github.com/ZuluPro/newsboard'
+default_app_config = 'newsboard.apps.NewsboardConfig'
