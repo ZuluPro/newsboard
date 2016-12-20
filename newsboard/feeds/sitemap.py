af56342d6eb78a3ec6915a709c6eaf3fd6fd7d80
ZuluPro <montheanthony@hotmail.com>
2016-12-20 07:23:32 -0500

    Added Sitemap Feed

---
+++ newsboard/feeds/sitemap.py
@@ -0,0 +1,27 @@
+try:
+    from urllib.request import urlopen, Request
+except ImportError:
+    from urllib2 import urlopen, Request
+from bs4 import BeautifulSoup
+from newsboard.feeds.base import BaseFeed
+from newsboard import settings
+
+
+class SitemapFeed(BaseFeed):
+    def _get_entry_url(self, entry):
+        return entry.find('loc').text
+
+    def _get_entries(self):
+        url = self.stream.remote_id
+        headers = {
+            'User-Agent': settings.USER_AGENT,
+        }
+        req = Request(url.encode('utf-8'), headers=headers)
+        xml = urlopen(req).read()
+        soup = BeautifulSoup(xml)
+        entries = soup.findAll("url")
+        return entries
+
+    def _get_post_attrs(self, entry):
+        attrs = {}
+        return attrs
